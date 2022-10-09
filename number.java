d40a52508661c18a7b7418b56881f8f645b5ecd3
yusagulgor <110815875+yusagulgor@users.noreply.github.com>
2022-10-09 23:29:12 +0300

    array i getirme
    ödev

---
+++ number.java
@@ -0,0 +1,19 @@
+package array;
+public class number {
+//private int[] numberrr = {1,2,3,4,5,6,7,8,9,10} ;
+private int[] numbers = {0,1,2,3,4,5,6,7,8,9,10};
+
+    public int[] getNumbers(){
+        System.out.println(numbers);
+        return numbers; 
+    }
+    public void setNumbers(int[] numbers) {   
+        this.numbers = numbers ;
+    }
+
+}
+
+
+    
+
+
---
+++ sifon.java
@@ -0,0 +1,10 @@
+package array;
+public class sifon {
+public static void main (String[] args){
+number numb = new number();
+numb.getNumbers();
+numb.setNumbers(null);
+System.out.println();
+//numb.setNumbers(8);
+//System.out.println(numb.getNumbers());
+}}
